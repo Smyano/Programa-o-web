5d30d4c2d7b16c3c860dd19bdca085c77ba6ddbd
yano <samirayanos2@gmail.com>
2025-04-19 20:32:30 -0300

    foto da pagina de home foi alterada

--- calculo.js
+++ calculo.js
@@ -1,14 +1,3 @@
-
-// function calcularRenda() {
-//   const fixa = parseFloat(document.getElementById('rendaFixa').value) || 0;
-//   const extra = parseFloat(document.getElementById('rendaExtra').value) || 0;
-//   const variavel = parseFloat(document.getElementById('rendaVariavel').value) || 0;
-
-//   const total = fixa + extra + variavel;
-
-//   const resultadoDiv = document.getElementById('resultado');
-//   resultadoDiv.innerText = `Renda Total: R$ ${total.toFixed(2)}`;
-// }
 function calcularGastos() {
   const fixa = parseFloat(document.getElementById('rendaFixa').value) || 0;
   const extra = parseFloat(document.getElementById('rendaExtra').value) || 0;
@@ -25,23 +14,6 @@ function calcularGastos() {
   const resultado1Div = document.getElementById('resultadoGastos');
   resultado1Div.innerText = `Gastos Totais: R$ ${total.toFixed(2)}`;
 }
-
-// function calcularTotal() {
-//   const fixa = parseFloat(document.getElementById('rendaFixa').value) || 0;
-//   const extra = parseFloat(document.getElementById('rendaExtra').value) || 0;
-//   const variavel = parseFloat(document.getElementById('rendaVariavel').value) || 0;
-//   const alimentacao = parseFloat(document.getElementById('alimentacao').value) || 0;
-//   const saude = parseFloat(document.getElementById('saude').value) || 0;
-//   const moradia = parseFloat(document.getElementById('moradia').value) || 0;
-//   const necessidades = parseFloat(document.getElementById('necessidades').value) || 0;
-//   const lazer = parseFloat(document.getElementById('lazer').value) || 0;
-
-//   const totalRenda = fixa + extra + variavel;
-//   const totalGastos = alimentacao + saude + moradia + necessidades + lazer;
-//   const total = totalRenda - totalGastos;
-//   const resultadoDiv = document.getElementById('resultadoTotal');
-//   resultadoDiv.innerText = `Saldo Total: R$ ${total.toFixed(2)}`;
-// }
 
 
 
