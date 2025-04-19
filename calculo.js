61e4ab7c2a66c69b42bbbf1c2139ce5788d2b8cc
yano <samirayanos2@gmail.com>
2025-04-19 19:05:43 -0300

    foi validado no js o valor da renda e os gastos do usuario, retornando o total das rendas menos o total de gastos

--- calculo.js
+++ calculo.js
@@ -1,15 +1,47 @@
 
-function calcularTotal() {
+// function calcularRenda() {
+//   const fixa = parseFloat(document.getElementById('rendaFixa').value) || 0;
+//   const extra = parseFloat(document.getElementById('rendaExtra').value) || 0;
+//   const variavel = parseFloat(document.getElementById('rendaVariavel').value) || 0;
+
+//   const total = fixa + extra + variavel;
+
+//   const resultadoDiv = document.getElementById('resultado');
+//   resultadoDiv.innerText = `Renda Total: R$ ${total.toFixed(2)}`;
+// }
+function calcularGastos() {
   const fixa = parseFloat(document.getElementById('rendaFixa').value) || 0;
   const extra = parseFloat(document.getElementById('rendaExtra').value) || 0;
   const variavel = parseFloat(document.getElementById('rendaVariavel').value) || 0;
+  const alimentacao = parseFloat(document.getElementById('alimentacao').value) || 0;
+  const saude = parseFloat(document.getElementById('saude').value) || 0;
+  const moradia = parseFloat(document.getElementById('moradia').value) || 0;
+  const necessidades = parseFloat(document.getElementById('necessidades').value) || 0;
+  const lazer = parseFloat(document.getElementById('lazer').value) || 0;
 
-  const total = fixa + extra + variavel;
+  const total = (fixa + extra + variavel) - (alimentacao + saude + moradia + necessidades + lazer);
 
-  const resultadoDiv = document.getElementById('resultado');
-  resultadoDiv.innerText = `Renda Total: R$ ${total.toFixed(2)}`;
+  
+  const resultado1Div = document.getElementById('resultadoGastos');
+  resultado1Div.innerText = `Gastos Totais: R$ ${total.toFixed(2)}`;
 }
 
+// function calcularTotal() {
+//   const fixa = parseFloat(document.getElementById('rendaFixa').value) || 0;
+//   const extra = parseFloat(document.getElementById('rendaExtra').value) || 0;
+//   const variavel = parseFloat(document.getElementById('rendaVariavel').value) || 0;
+//   const alimentacao = parseFloat(document.getElementById('alimentacao').value) || 0;
+//   const saude = parseFloat(document.getElementById('saude').value) || 0;
+//   const moradia = parseFloat(document.getElementById('moradia').value) || 0;
+//   const necessidades = parseFloat(document.getElementById('necessidades').value) || 0;
+//   const lazer = parseFloat(document.getElementById('lazer').value) || 0;
+
+//   const totalRenda = fixa + extra + variavel;
+//   const totalGastos = alimentacao + saude + moradia + necessidades + lazer;
+//   const total = totalRenda - totalGastos;
+//   const resultadoDiv = document.getElementById('resultadoTotal');
+//   resultadoDiv.innerText = `Saldo Total: R$ ${total.toFixed(2)}`;
+// }
 
 
 
