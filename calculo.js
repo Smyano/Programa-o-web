73cc515e67eaf7eb0ba9c2a5fc73e72944b74bc2
yano <samirayanos2@gmail.com>
2025-04-20 15:48:07 -0300

    ajustado os valores na categoria de gastos, também foi colocado para que quando o usuário clicar em calcular renda, retorne a soma total das rendas

--- calculo.js
+++ calculo.js
@@ -50,39 +50,43 @@ function calcularGastos() {
   const resultado1Div = document.getElementById('resultadoGastos');
   const totalFinal = totalReceita - sobra;
 
-  resultado1Div.innerText = `Total de Gastos: R$ ${totalFinal.toFixed(2)}\nValor Guardado: R$ ${sobra.toFixed(2)}`;
+  resultado1Div.innerText =
+    `Total da Renda: R$ ${totalReceita.toFixed(2)}\n` +
+    `Total de Gastos: R$ ${totalFinal.toFixed(2)}\n` +
+    `Valor Guardado: R$ ${sobra.toFixed(2)}`;
 }
-function buscarRenda() {
-  const estado = document.getElementById("estado").value;
-  const resultadoDiv = document.getElementById("resultadoRenda");
 
-  if (!estado) {
-    resultadoDiv.innerText = "Por favor, selecione um estado.";
-    return;
-  }
+// function buscarRenda() {
+//   const estado = document.getElementById("estado").value;
+//   const resultadoDiv = document.getElementById("resultadoRenda");
 
-  const rendaEstados = {
-    "12": 950.50,
-    "27": 890.70,
-    "13": 1000.20,
-    "29": 1100.30,
-    "23": 950.90,
-    "53": 2800.00,
-    "32": 1600.00,
-    "52": 1700.00,
-    "31": 1800.00,
-    "33": 2300.00,
-    "35": 2500.00,
+//   if (!estado) {
+//     resultadoDiv.innerText = "Por favor, selecione um estado.";
+//     return;
+//   }
 
-  };
+//   const rendaEstados = {
+//     "12": 950.50,
+//     "27": 890.70,
+//     "13": 1000.20,
+//     "29": 1100.30,
+//     "23": 950.90,
+//     "53": 2800.00,
+//     "32": 1600.00,
+//     "52": 1700.00,
+//     "31": 1800.00,
+//     "33": 2300.00,
+//     "35": 2500.00,
 
-  const renda = rendaEstados[estado];
+//   };
+
+//   const renda = rendaEstados[estado];
 
 
 
-  if (renda) {
-    resultadoDiv.innerText = `Renda média é de: R$ ${renda.toFixed(2).replace('.', ',')}`;
-  } else {
-    resultadoDiv.innerText = "Dados não disponíveis.";
-  }
-}
+//   if (renda) {
+//     resultadoDiv.innerText = `Renda média é de: R$ ${renda.toFixed(2).replace('.', ',')}`;
+//   } else {
+//     resultadoDiv.innerText = "Dados não disponíveis.";
+//   }
+// }
